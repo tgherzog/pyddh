3f867e56d484cae2bf48a5b5a44f78f91e80dc7d
Tim Herzog <therzog1@worldbank.org>
2018-10-18 19:58:07 -0400

    Also check the CKAN url field

--- eex-check.py
+++ eex-check.py
@@ -5,6 +5,7 @@ import re
 # quick & dirty script to scan EEX and look for links that may already be in the data catalog or DECDG systems
 
 url = 'https://energydata.info/api/3/action/package_list'
+regex_str = '^https?://(data|databank|datacatalog).worldbank.org/';
 response = requests.get(url).json()
 for id in response['result']:
     url = 'https://energydata.info/api/3/action/package_show?id={}'.format(id)
@@ -12,8 +13,11 @@ for id in response['result']:
     dataset = response['result']
     try:
         if dataset.get('organization',{}).get('name','') == 'world-bank-grou':
+            if re.match(regex_str, dataset.get('url', '')):
+                print 'Dataset URL: {} {}'.format(id, dataset['url'])
+
             for i in dataset.get('resources',[]):
                 if re.match('^https?://(data|databank|datacatalog).worldbank.org/', i['url']):
-                    print '{} {}'.format(id['name'], i['url'])
+                    print 'Resource URL: {} {}'.format(id, i['url'])
     except:
         raise
