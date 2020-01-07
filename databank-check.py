5f96650db706fce3a545be96e93cb10fd91d6817
Tim Herzog <therzog1@worldbank.org>
2020-01-07 13:05:59 -0500

    databank-check.py now calculates the date discrepancy

--- databank-check.py
+++ databank-check.py
@@ -8,14 +8,12 @@ import wbgapi as wb
 import ddh
 import sys
 import csv
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from datetime import datetime
 
 # TODO: convert date fields to dates and show difference in an extra column
 
 writer = csv.writer(sys.stdout)
-writer.writerow(['NID','DDH_TITLE', 'DDH_MODIFIED', 'DBANK_ID', 'DBANK_TITLE', 'DBANK_MODIFIED'])
+writer.writerow(['NID','DDH_TITLE', 'DDH_MODIFIED', 'DBANK_ID', 'DBANK_TITLE', 'DBANK_MODIFIED', 'DISCREPANCY'])
 
 ddh.load('datacatalog.worldbank.org')
 for k,v in ddh.dataset.search(['field_wbddh_modified_date', 'field_ddh_harvest_sys_id'], {'field_ddh_harvest_src': 'Indicators API'}):
@@ -23,5 +21,12 @@ for k,v in ddh.dataset.search(['field_wbddh_modified_date', 'field_ddh_harvest_s
     mod_date    = v['field_wbddh_modified_date']['und'][0]['value'] if v['field_wbddh_modified_date'] else ''
     mod_date = mod_date.split(' ')[0]
     dbank = wb.source.get(database_id)
+    delta = ''
+    try:
+        date1 = datetime.strptime(dbank['lastupdated'], '%Y-%m-%d')
+        date2 = datetime.strptime(mod_date, '%Y-%m-%d')
+        delta = (date2-date1).days
+    except:
+        pass
 
-    writer.writerow([k, v['title'], mod_date, database_id, dbank['name'], dbank['lastupdated']])
+    writer.writerow([k, v['title'], mod_date, database_id, dbank['name'], dbank['lastupdated'], delta])
